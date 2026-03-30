2c26df50c8a770b2b13426bf023beca2209a320f
xiaomi86pro <xiaomi86pro@gmail.com>
2026-03-30 22:25:22 +0700

    xcv

--- StepbyStep/BackUp/GetTree.ps1
+++ StepbyStep/BackUp/GetTree.ps1
@@ -1,16 +1,20 @@
 $outputFile = "project_tree.txt"
-$ignore = @(".git","node_modules","build","dist")
+
+# Ignore theo PATH (quan trọng)
+$ignorePattern = '\\.git\\|\\node_modules\\|\\build\\|\\dist\\|\\.next\\|\\.turbo\\|\\.cache\\|coverage\\'
+# Chỉ giữ file làm việc
 $extensions = @(".ts",".tsx",".js",".jsx",".css",".scss",".json",".md",".html",".env",".yml",".yaml",".png",".jpg",".svg")
 
 function Show-Tree($path, $indent="") {
-    Get-ChildItem -LiteralPath $path | Where-Object { $ignore -notcontains $_.Name } | Sort-Object PSIsContainer -Descending | ForEach-Object {
+    Get-ChildItem -LiteralPath $path | Where-Object { $_.FullName -notmatch $ignorePattern } |
+    Sort-Object PSIsContainer -Descending | ForEach-Object {
+
         if ($_.PSIsContainer) {
-            "$indent+-- $_" | Out-File -FilePath $outputFile -Append
+            "$indent+-- $($_.Name)" | Out-File -FilePath $outputFile -Append
             Show-Tree $_.FullName "$indent|   "
         } else {
-            # Lọc file theo extension
             if ($extensions -contains $_.Extension) {
-                "$indent+-- $_" | Out-File -FilePath $outputFile -Append
+                "$indent+-- $($_.Name)" | Out-File -FilePath $outputFile -Append
             }
         }
     }
